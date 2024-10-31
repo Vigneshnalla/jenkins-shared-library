22de8ae7ab27563dbb8bd63cfbba53bf40fd17e9
vignesh nalla <vigneshnalla999@gmail.com>
2024-10-31 15:33:44 +0530

    first commit

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -57,19 +57,19 @@ def call(Map configMap){
                 }
             }
 
-            // stage('Deploy'){
-            //     steps{
+            stage('Deploy'){
+                steps{
             //         script{
             //             releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
             //             if(releaseExists.isEmpty()){
             //                 echo "${component} not installed yet, first time installation"
-            //                 sh"""
-            //                     aws eks update-kubeconfig --region ${region} --name ${project}-dev
-            //                     cd helm
-            //                     sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-            //                     helm install ${component} -n ${project} .
+                            sh"""
+                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
+                                cd helm
+                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+                                helm install ${component} -n ${project} .
             //                 """
-            //             }
+                        }
             //             else{
             //                 echo "${component} exists, running upgrade"
             //                 sh"""
@@ -81,7 +81,7 @@ def call(Map configMap){
             //             }
             //         }
             //     }
-            // }
+            }
             // stage('Verify Deployment'){
             //     steps{
             //         script{
