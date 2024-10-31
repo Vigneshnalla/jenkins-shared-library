dd2a81a6425a4df525c8b09ef470b35b4648aa84
vignesh nalla <vigneshnalla999@gmail.com>
2024-10-31 15:24:18 +0530

    first commit

--- vars/nodejsEKS.groovy
+++ vars/nodejsEKS.groovy
@@ -57,61 +57,61 @@ def call(Map configMap){
                 }
             }
 
-            stage('Deploy'){
-                steps{
-                    script{
-                        releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
-                        if(releaseExists.isEmpty()){
-                            echo "${component} not installed yet, first time installation"
-                            sh"""
-                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                                cd helm
-                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                                helm install ${component} -n ${project} .
-                            """
-                        }
-                        else{
-                            echo "${component} exists, running upgrade"
-                            sh"""
-                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                                cd helm
-                                sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
-                                helm upgrade ${component} -n ${project} .
-                            """
-                        }
-                    }
-                }
-            }
-            stage('Verify Deployment'){
-                steps{
-                    script{
-                        rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n ${project} --timeout=1m || true", returnStdout: true).trim()
-                        if(rollbackStatus.contains('successfully rolled out')){
-                            echo "Deployment is successfull"
-                        }
-                        else{
-                            echo "Deployment is failed, performing rollback"
-                            if(releaseExists.isEmpty()){
-                                error "Deployment failed, not able to rollback, since it is first time deployment"
-                            }
-                            else{
-                                sh """
-                                aws eks update-kubeconfig --region ${region} --name ${project}-dev
-                                helm rollback backend -n ${project} 0
-                                sleep 60
-                                """
-                                rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n expense --timeout=2m || true", returnStdout: true).trim()
-                                if(rollbackStatus.contains('successfully rolled out')){
-                                    error "Deployment is failed, Rollback is successfull"
-                                }
-                                else{
-                                    error "Deployment is failed, Rollback is failed"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            // stage('Deploy'){
+            //     steps{
+            //         script{
+            //             releaseExists = sh(script: "helm list -A --short | grep -w ${component} || true", returnStdout: true).trim()
+            //             if(releaseExists.isEmpty()){
+            //                 echo "${component} not installed yet, first time installation"
+            //                 sh"""
+            //                     aws eks update-kubeconfig --region ${region} --name ${project}-dev
+            //                     cd helm
+            //                     sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+            //                     helm install ${component} -n ${project} .
+            //                 """
+            //             }
+            //             else{
+            //                 echo "${component} exists, running upgrade"
+            //                 sh"""
+            //                     aws eks update-kubeconfig --region ${region} --name ${project}-dev
+            //                     cd helm
+            //                     sed -i 's/IMAGE_VERSION/${appVersion}/g' values.yaml
+            //                     helm upgrade ${component} -n ${project} .
+            //                 """
+            //             }
+            //         }
+            //     }
+            // }
+            // stage('Verify Deployment'){
+            //     steps{
+            //         script{
+            //             rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n ${project} --timeout=1m || true", returnStdout: true).trim()
+            //             if(rollbackStatus.contains('successfully rolled out')){
+            //                 echo "Deployment is successfull"
+            //             }
+            //             else{
+            //                 echo "Deployment is failed, performing rollback"
+            //                 if(releaseExists.isEmpty()){
+            //                     error "Deployment failed, not able to rollback, since it is first time deployment"
+            //                 }
+            //                 else{
+            //                     sh """
+            //                     aws eks update-kubeconfig --region ${region} --name ${project}-dev
+            //                     helm rollback backend -n ${project} 0
+            //                     sleep 60
+            //                     """
+            //                     rollbackStatus = sh(script: "kubectl rollout status deployment/backend -n expense --timeout=2m || true", returnStdout: true).trim()
+            //                     if(rollbackStatus.contains('successfully rolled out')){
+            //                         error "Deployment is failed, Rollback is successfull"
+            //                     }
+            //                     else{
+            //                         error "Deployment is failed, Rollback is failed"
+            //                     }
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
             
             /* stage('Nexus Artifact Upload'){
                 steps{
